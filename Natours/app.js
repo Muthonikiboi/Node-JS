2fd26ceb5026895c9137935d223db92cc6125975
Muthonikiboi <kiboijoye254@gmail.com>
2024-01-12 01:57:16 +0300

    ADD:API Handling GET Method

--- Natours/app.js
+++ Natours/app.js
@@ -1,21 +1,38 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-//get method
-app.get("/", function (req, res) {
-  //next lines speifies the status code-use 200 for okay
-  //res.status(200), res.send("send request to homepage");
+// //get method
+// app.get("/", function (req, res) {
+//   //next lines speifies the status code-use 200 for okay
+//   //res.status(200), res.send("send request to homepage");
+//   res.status(200),
+//     res.json({
+//       message: "send information to homepage",
+//       app: "Natours",
+//     });
+// });
+
+// //post method
+// app.post("/", function (req, res) {
+//   res.send("you can post to the endpoint...");
+// });
+
+//API Handling GET
+const tour = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`) //${__dirname} --shows that it is the current directory
+);
+
+app.get("/api/v1/tours", function (req, res) {
   res.status(200),
     res.json({
-      message: "send information to homepage",
-      app: "Natours",
+      status: "success",
+      results: tour.length,
+      data: {
+        tours: tour,
+      },
     });
-});
-
-//post method
-app.post("/", function (req, res) {
-  res.send("you can post to the endpoint...");
 });
 
 const port = 3000;
