0644aa3af7ec46dc4dbb14dd93583177cc5c7a75
Muthonikiboi <kiboijoye254@gmail.com>
2024-01-21 23:57:21 +0300

    ADD:Chaining Multiple Middleware Functions

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -22,6 +22,30 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+//add a middleware to check Id
+exports.checkId = function (req, res, next, val) {
+  console.log(`tour id is:${val}`);
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "Fail",
+      message: "Invalid ID",
+    });
+  }
+  next();
+};
+
+exports.checkBody = function (req, res, next) {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: "Fail",
+      message: "Bad request",
+    });
+    next();
+  }
+};
+
 exports.getAllTours = function (req, res) {
   console.log(req.requestTime);
   res.status(200),
@@ -36,16 +60,18 @@ exports.getAllTours = function (req, res) {
 };
 
 exports.getTour = function (req, res) {
-  // convert string to number
-  const id = req.params.id * 1;
+  // // convert string to number
+  // const id = req.params.id * 1;
 
-  // invalid ID for id greater than max no of tours
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "Fail",
-      message: "Invalid ID",
-    });
-  }
+  // // invalid ID for id greater than max no of tours
+  // if (id > tours.length) {
+  //   return res.status(404).json({
+  //     status: "Fail",
+  //     message: "Invalid ID",
+  //   });
+  // }
+
+  const id = req.params.id * 1;
 
   // find the tour by id
   const tour = tours.find((el) => el.id === id);
@@ -94,15 +120,15 @@ exports.createTour = function (req, res) {
 };
 
 exports.updateTour = function (req, res) {
-  //we can implelment the Id below when it is actually valid
-  const id = req.params.id * 1;
+  // //we can implelment the Id below when it is actually valid
+  // const id = req.params.id * 1;
 
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "Fail",
-      message: "Invalid ID",
-    });
-  }
+  // if (id > tours.length) {
+  //   return res.status(404).json({
+  //     status: "Fail",
+  //     message: "Invalid ID",
+  //   });
+  // }
 
   res.status(200).json({
     status: "success",
@@ -113,15 +139,6 @@ exports.updateTour = function (req, res) {
 };
 
 exports.deleteTour = function (req, res) {
-  const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "Fail",
-      message: "Invalid ID",
-    });
-  }
-
   res.status(204).json({
     status: "success",
     data: null,
--- Natours/routes/tourRoutes.js
+++ Natours/routes/tourRoutes.js
@@ -5,10 +5,20 @@ const tourController = require("./../controllers/tourController");
 
 const router = express.Router();
 
+//PARAM MIDDLEWARE-runs only for a certain parameter
+//The parameter it wants to run for is id
+//since it a middleware has next function
+//val-value of the parameter in question
+router.param("id", tourController.checkId);
+
+//Create checkbody middleware function
+//check if body contains the name and price property
+//if not,send back 400(bad request)
+
 router
   .route("/")
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route("/:id")
