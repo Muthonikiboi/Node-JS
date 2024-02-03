89b57ec1754be3ade8bbf592f103d7f9339d469c
Muthonikiboi <kiboijoye254@gmail.com>
2024-02-03 23:56:17 +0300

    ADD:Connecting to MongoDB

--- Natours/server.js
+++ Natours/server.js
@@ -10,15 +10,20 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
+  //how to connect to the local database
+  //.connect(process.env.DATABASE_LOCAL,{
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
-  .then((con) => {
-    //console.log(con.connections);
-    console.log("DB CONNECTION SUCCESSFUL");
-  });
+  .then(() => console.log("DB Connection Successful"));
+// .then((con) => {
+//   //con is the result value of the promise
+//   console.log(con.connections);
+//   console.log("DB CONNECTION SUCCESSFUL");
+// });
 
 const port = 3000;
 //callback function to output "" when the server starts running
